fab10499a0a66dda68d84616ab67145658d8f335
Mheyrie <omota4fidelity@gmail.com>
2025-05-24 18:09:54 +0000

    feat: implement PatientForm component and update Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { ModeToggle } from "@/components/ModeToggle";
 import Image from "next/image";
 
-
 export default function Home() {
   return (
-    <div className="flex h-screen max-h-screen"><ModeToggle/>
+    <div className="flex h-screen max-h-screen">
+      <ModeToggle />
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-         <div className="flex justify-center mb-12">
+          {/* Logo  */}
+          <div className="flex mb-12">
             <Image
               src="/assets/icons/logo.png"
               alt="Patient"
@@ -16,11 +18,15 @@ export default function Home() {
               className="w-fit h-10"
             />
             <span className="text-sm italic mt-2 font-semibold">We Care</span>
-         </div>
+          </div>
+          {/* Form  */}
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© {new Date().getFullYear()} We Care. All rights reserved.</p>
+          </div>
         </div>
       </section>
-      
-      
-      </div>
+    </div>
   );
 }
