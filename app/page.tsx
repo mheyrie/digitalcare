0a88be7b2cf70f1a1042e8798a4c84f7638a8317
Mheyrie <omota4fidelity@gmail.com>
2025-05-24 17:57:06 +0000

    Add favicon, next, and vercel SVG icons
    - Added a new favicon.ico file to the public directory.
    - Introduced next.svg for navigation purposes.
    - Included vercel.svg for branding in the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 import { ModeToggle } from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="">hello</h1>
-      <Button>Click me</Button>
-      <ModeToggle/>
+    <div className="flex h-screen max-h-screen"><ModeToggle/>
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+         <div className="flex justify-center mb-12">
+            <Image
+              src="/assets/icons/logo.png"
+              alt="Patient"
+              width={1000}
+              height={1000}
+              className="w-fit h-10"
+            />
+            <span className="text-sm italic mt-2 font-semibold">We Care</span>
+         </div>
+        </div>
+      </section>
+      
+      
       </div>
   );
 }
