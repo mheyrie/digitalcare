3fc1c8ee49d9650f48de3eae2ec170f2f188463b
Mheyrie <omota4fidelity@gmail.com>
2025-05-24 16:31:02 +0000

    Refactor styles and layout; integrate Tailwind CSS, update dependencies, and enhance utility functions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css"; 
+import {Plus_Jakarta_Sans} from "next/font/google";
+import { cn } from "@/lib/utils";
 
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -27,8 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}      >
         {children}
       </body>
     </html>
