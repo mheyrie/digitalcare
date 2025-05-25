f30a48b2d339f4c4e81f92c88192196f1f8e4c84
Mheyrie <omota4fidelity@gmail.com>
2025-05-25 01:02:50 +0000

    feat: refactor PatientForm to simplify structure and add CustomFormField component

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,16 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -38,23 +29,14 @@ const PatientForm = () => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi There 👋👋🏼</h1>
+          <p className="text-dark-300 dark:text-dark-700">
+            Schedule your first appointment
+          </p>
+        </section>
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
