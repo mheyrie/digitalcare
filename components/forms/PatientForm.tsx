d1b00956b0fa2ce5e91638fe6973cc18c93b5bf9
Mheyrie <omota4fidelity@gmail.com>
2025-05-25 16:09:50 +0000

    feat: implement UserFormValidation and add SubmitButton component for form submission

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -2,10 +2,13 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { set, z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -17,23 +20,30 @@ export enum FormFieldType {
   SKELETON = "skeleton",
 }
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
-
 const PatientForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof UserFormValidation>) {
+    const { name, email, phone } = values;
+    setIsLoading(true);
+    try {
+      const userData = { name, email, phone };
+
+      const user = await createUser(userData);
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
@@ -74,11 +84,9 @@ const PatientForm = () => {
           label="Phone Number:"
           placeholder="(555) 123-4355"
           control={form.control}
-                />
+        />
 
-        <Button type="submit" variant="outline">
-          Submit
-        </Button>
+        <SubmitButton isLoading={isLoading}> Get Started</SubmitButton>
       </form>
     </Form>
   );
