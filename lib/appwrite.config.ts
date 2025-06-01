4e8a048de0e9d51bf3d748ec1a27310a791347f0
Mheyrie <omota4fidelity@gmail.com>
2025-06-01 23:07:09 +0000

    fix: add messaging export to appwrite configuration

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -21,3 +21,4 @@ client.setEndpoint(ENDPOINT!)
 
   export const databases = new sdk.Databases(client)
   export const storage = new sdk.Storage(client);
+  export const messaging = new sdk.Messaging(client);
