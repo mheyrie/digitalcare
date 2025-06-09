bf96cce511b949c1b58385a1ef68bada50a2d903
Mheyrie <omota4fidelity@gmail.com>
2025-06-09 22:57:38 +0000

    UODSTED

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -42,7 +42,7 @@ const PatientForm = () => {
     try {
       const userData = { name, email, phone };
 
-      // const user = await createUser(userData);
+      const user = await createUser(userData);
       if (user) router.push(`/patients/${user.$id}/register`);
 
 
