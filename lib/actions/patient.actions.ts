21f0d79c34ceb37e71bc3a9ba5ecb72ca8a7860b
Mheyrie <omota4fidelity@gmail.com>
2025-06-11 17:38:38 +0000

    updated

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,17 +1,12 @@
-import { Query } from "node-appwrite"
+import { Query } from "node-appwrite";
 
 export const createUser = async (user: CreateUserParams) => {
-    try {
-        
-       
-        
-      
-    } catch (error) {
-        if(error && error?.code === 409){
-const existingUser = await users.list([
-    Query.equal('email', [user.email]),
-])
-        }
+  try {
+  } catch (error) {
+    if (error && error?.code === 409) {
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
     }
-
-}
+  }
+};
