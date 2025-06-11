2eaad9bd3c123e1f30f0bbdca02882c7b09bb2e2
Mheyrie <omota4fidelity@gmail.com>
2025-06-11 16:31:33 +0000

    updated

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,17 +1,11 @@
 export const createUser = async (user: CreateUserParams) => {
-    //REVIEW - This function is used to create a new user in the system.
-    const response = await fetch("/api/users", {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-    });
-    
-    if (!response.ok) {
-        throw new Error("Failed to create user");
+    try {
+        
+       
+        
+      
+    } catch (error) {
+        
     }
-    
-    return response.json();
 
 }
