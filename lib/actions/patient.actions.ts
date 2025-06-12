56b03fd03091cb50eb5a8970ecbbe1b6c3380d95
Mheyrie <omota4fidelity@gmail.com>
2025-06-12 18:42:13 +0000

    Refactor createUser function to handle user creation and return existing user if email already exists

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,13 +1,20 @@
-import { Query } from "node-appwrite";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
-  } catch (error) {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      user.name,
+      undefined
+    );
+  } catch (error: any) {
     if (error && error?.code === 409) {
-      const existingUser = await users.list([
-        Query.equal("email", [user.email]),
-      ]);
-      return existingUser?.users[0]
+      const documents = await users.list([Query.equal("email", [user.email])]);
+      return documents?.users[0];
     }
   }
 };
