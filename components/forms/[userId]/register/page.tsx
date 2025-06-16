41356590cf8b96957d9ec3605ab1dfe36cd249bb
Mheyrie <omota4fidelity@gmail.com>
2025-06-16 22:48:18 +0000

    Remove unnecessary whitespace in Register component layout

--- components/forms/[userId]/register/page.tsx
+++ components/forms/[userId]/register/page.tsx
@@ -2,7 +2,7 @@ const Register = () => {
   return (
     <div className="flex h-screen max-h-screen">
         <section className="remove-scrollbar container">
-            
+            <div className="sub-container max-w-[860px] flex-1"></div>
         </section>
     </div>
   )
