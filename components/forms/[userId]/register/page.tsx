92a8bebb134c463951d0b7affa561cca1aab16a6
Mheyrie <omota4fidelity@gmail.com>
2025-06-16 22:50:28 +0000

    Fix formatting and ensure consistent code style in Register component

--- components/forms/[userId]/register/page.tsx
+++ components/forms/[userId]/register/page.tsx
@@ -1,20 +1,20 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const Register = () => {
   return (
     <div className="flex h-screen max-h-screen">
-        <section className="remove-scrollbar container">
-            <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
-                <Image
-                    src="/assets/icon/logo.svg"
-                    alt="Digital care"
-                    width={1000}
-                    height={1000}
-                    className="mb-12 h-10 w-fit"
-                />
-            </div>
-        </section>
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+          <Image
+            src="/assets/icon/logo.svg"
+            alt="Digital care"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+      </section>
     </div>
-  )
-}
-export default Register
+  );
+};
+export default Register;
