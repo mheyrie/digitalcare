4bd5d57be64a1d8bcc7e2d00c94d0b1cb154a5ac
Mheyrie <omota4fidelity@gmail.com>
2025-06-18 22:58:16 +0000

    Update welcome message in RegisterForm component for improved user engagement

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -46,7 +46,7 @@ const RegisterForm = ({user}: {user:User}) => {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
         <section className="space-y-4">
-          <h1 className="header">Welcome 👋🏼</h1>
+          <h1 className="header">Welcome, Happy to have you here 👋🏼</h1>
           <p className="text-dark-300 dark:text-dark-700">
            Let us know a bit about you to get started
           </p>
