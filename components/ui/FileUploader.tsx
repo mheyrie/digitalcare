9523377178243e0fc2029be71e3f1be82c2d57ae
Mheyrie <omota4fidelity@gmail.com>
2025-08-20 22:51:26 +0000

    add instructional text and supported formats to FileUploader component

--- components/ui/FileUploader.tsx
+++ components/ui/FileUploader.tsx
@@ -22,6 +22,12 @@ type FileUploaderProps = {
        <Image src={convertFileToUrl(files[0])} alt="Uploaded file" width={1000} height={1000} className="max-h-[400px] overflow-hidden object-cover" />
       ) : <>
       <Image src="/assets/icons/upload.svg" alt="Upload Icon" width={400} height={400} className="max-h-[400px] overflow-hidden object-cover" />
+      <div className="file-upload_label">
+        <p className="text-14-regular">
+            <span className="text-green-500">Click to upload</span> or drag and drop files here
+        </p>
+        <p className="text-12-regular text-gray-500">Supported formats: JPG, PNG, GIF(max 800*400)</p>
+      </div>
       </>}
       {isDragActive ? (
         <p>Drop the files here ...</p>
