707f7e05045b0ba168aea34d8fbbe9193ad94ed3
Mheyrie <omota4fidelity@gmail.com>
2025-08-21 19:25:29 +0000

    feat: implement AppointmentForm component for scheduling appointments and update NewAppointment layout

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,6 +1,49 @@
+
+import AppointmentForm from "@/components/forms/AppointmentForm"
+import { ModeToggle } from "@/components/ModeToggle"
+import Image from "next/image"
+
+
 const NewAppointment = () => {
   return (
-    <div>NewAppointment</div>
+   <div className="flex h-screen max-h-screen">
+
+      <ModeToggle />
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          {/* Logo  */}
+          <div className="flex mb-12">
+            <Image
+              src="/assets/icons/logo.png"
+              alt="Patient"
+              width={1000}
+              height={1000}
+              className="w-fit h-10"
+            />
+            <span className="text-sm italic mt-2 font-semibold">We Care</span>
+          </div>
+
+          {/* Form  */}
+          <AppointmentForm />
+
+           <p className="justify-items-end text-dark-600 xl:text-left">
+              © {new Date().getFullYear()} We Care. All rights reserved.
+            </p>
+           
+          </div>
+        
+      </section>
+      <Image
+        width={1000}
+        height={1000}
+        src="/assets/images/appointment-img.png"
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+        loading="eager"
+        fetchPriority="high"
+        priority
+      />
+    </div>
   )
 }
 export default NewAppointment
