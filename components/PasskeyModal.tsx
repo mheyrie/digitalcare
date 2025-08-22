b82c37d9d496555a4ef8eb21dee55e89e4dec80e
Mheyrie <omota4fidelity@gmail.com>
2025-08-22 01:19:16 +0000

    feat: implement PasskeyModal component with alert dialog functionality

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -1,6 +1,37 @@
+"use client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useState } from "react";
+
 const PasskeyModal = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>PasskeyModal</div>
-  )
-}
-export default PasskeyModal
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+export default PasskeyModal;
