d2e62e398ca74c3176be0b8345b47ea18d0cbdea
Mheyrie <omota4fidelity@gmail.com>
2025-08-22 00:19:51 +0000

    feat: add SuccessPage component to display appointment creation confirmation

---
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const SuccessPage = () => {
+  return (
+    <div className="flex h-screen max-h-screen px-[5%]">
+        <div className="success-img">
+            <Link href='/'>
+                <Image
+                    src='/assets/icons/logo.png'
+                    alt='Success'
+                    className="h-10 w-fit"
+                    height={1000}
+                    width={1000}
+                />
+            </Link>
+           <section className="flex flex-col items-center">  <Image
+                    src='/assets/gifs/success.gif'
+                    alt='Success'
+                    className="h-10 w-fit"
+                    height={300}
+                    width={280}
+                />
+                <h2 className="header mb-6 max-w-[600px] text-center">Your  <span className="text-green-500">appointment request </span>has been successfully created!
+              
+                </h2>
+                <p className="text-center">We will get back to you shortly with the details.</p>
+           </section>
+           <section className="request-details">
+            <p className="">Requested appointment details:</p>
+            <div className="flex items-center gap-3">
+                {/* <Image
+                    src='/assets/icons/calendar.png'
+                    alt='Calendar'
+                    className="h-6 w-6"
+                    height={24}
+                    width={24}
+                /> */}
+                <p className="">March 15, 2023 at 10:00 AM</p>
+            </div>
+           </section>
+        </div>
+    </div>
+  )
+}
+export default SuccessPage
