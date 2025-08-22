938753d549f8fbb170dd1cd38d1d0a5efca5be30
Mheyrie <omota4fidelity@gmail.com>
2025-08-22 01:16:01 +0000

    feat: add PasskeyModal component and integrate it into Home for admin users

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import PatientForm from "@/components/forms/PatientForm";
 import { ModeToggle } from "@/components/ModeToggle";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* //TODO - OTP verification || pass key modal */}
-      <ModeToggle />
+      <ModeToggle /> {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           {/* Logo  */}
