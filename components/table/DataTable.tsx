84ed00648b59ae292114f8fbf12fb54a328769b4
Mheyrie <omota4fidelity@gmail.com>
2025-08-22 14:28:57 +0000

    feat: enhance DataTable component with improved styling and pagination controls; add icons for navigation buttons

--- components/table/DataTable.tsx
+++ components/table/DataTable.tsx
@@ -17,6 +17,7 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import { Button } from "../ui/button"
+import Image from "next/image"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -35,9 +36,9 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="overflow-hidden rounded-md border">
-      <Table>
-        <TableHeader>
+    <div className="data-table">
+      <Table className="shad-table">
+        <TableHeader className="bg-dark-200">
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
@@ -61,6 +62,7 @@ export function DataTable<TData, TValue>({
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
+                className="shad-table-row"
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
@@ -78,22 +80,22 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="table-actions">
         <Button
           variant="outline"
           size="sm"
           onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
+          disabled={!table.getCanPreviousPage()} className="shad-gray-btn"
         >
-          Previous
+         <Image src="/assets/icons/arrow.svg" alt="Previous" width={24} height={24} />
         </Button>
-        <Button
+         <Button
           variant="outline"
           size="sm"
           onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
+          disabled={!table.getCanNextPage()} className="shad-gray-btn"
         >
-          Next
+         <Image src="/assets/icons/arrow.svg" alt="Next" width={24} height={24} className="rotate-180" />
         </Button>
       </div>
     </div>
