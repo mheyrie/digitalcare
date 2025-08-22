2c1e89728af8d6f4d177702463bee9785315dbac
Mheyrie <omota4fidelity@gmail.com>
2025-08-22 01:25:00 +0000

    feat: update PasskeyModal to include close functionality and improve admin access verification layout

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -8,19 +8,37 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
+ 
 } from "@/components/ui/alert-dialog";
+import Image from "next/image";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 const PasskeyModal = () => {
-  const [open, setOpen] = useState(false);
+    const router = useRouter();
+  const [open, setOpen] = useState(true);
+
+  const closeModal = () => {
+    setOpen(false);
+    router.push('/');
+  };
 
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
-      <AlertDialogTrigger>Open</AlertDialogTrigger>
-      <AlertDialogContent>
+      
+      <AlertDialogContent className="shad-alert-dialog">
         <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogTitle className="flex items-start justify-between">Admin Access Verification
+
+            <Image
+              src="/assets/icons/close.svg"
+              alt="Close"
+              width={20}
+              height={20}
+              className="ml-2 cursor-pointer"
+              onClick={() => closeModal()}
+            />
+          </AlertDialogTitle>
           <AlertDialogDescription>
             This action cannot be undone. This will permanently delete your
             account and remove your data from our servers.
