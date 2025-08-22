3f4b40ccbfb655ad697c3b51ed81a3642a1768a8
Mheyrie <omota4fidelity@gmail.com>
2025-08-22 02:45:34 +0000

    feat: implement Admin dashboard layout with StatCard components for appointment management

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,51 @@
+import StatCard from "@/components/StatCard";
+import Image from "next/image";
+import Link from "next/link";
+
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
-export default Admin
+    <div className="mx-auto flex max-w-7xl flex-col space-y-14">
+      <header className="admin-header">
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/assets/icons/logo.png"
+            alt="Logo"
+            width={162}
+            height={32}
+            className="h-8 w-fit"
+          />
+        </Link>
+        <p className="text-16-semibold">Admin Dashboard</p>
+      </header>
+      <main className="admin-main">
+        <section className="w-full space-y-4">
+          <h1 className="header">Welcome 👋🏼</h1>
+          <p className="text-dark-700">
+            Start the day with managing new appointments
+          </p>
+        </section>
+        <section className="admin-stat">
+          <StatCard
+            type="appointments"
+            count={5}
+            label="Scheduled Appointments"
+            icon="/assets/icons/appointments.svg"
+          />
+          <StatCard
+            type="pending"
+            count={10}
+            label="Pending Appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard
+            type="cancelled"
+            count={2}
+            label="Cancelled Appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
+        </section>
+      </main>
+    </div>
+  );
+};
+export default Admin;
